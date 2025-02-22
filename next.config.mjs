bce4661f91f7aebe1a4c18462bd77448e61423cd
tanojctg <126942194+tanojctg@users.noreply.github.com>
2025-02-22 23:50:07 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['wallpapercave.com' , 'crafatar.com'], // Specify the allowed domain
+    },
+  };
+  
+  export default nextConfig;
+  
